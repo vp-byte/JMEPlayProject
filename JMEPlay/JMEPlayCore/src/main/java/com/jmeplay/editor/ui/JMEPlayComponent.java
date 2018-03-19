1ce2880ec54e9cb8307177bfab2364e05f489cd4
Vladimir Petrenko <vladipetrenko@vp-byte.de>
2018-03-19 10:45:36 +0100

    Change to MIT License

--- JMEPlay/JMEPlayCore/src/main/java/com/jmeplay/editor/ui/JMEPlayComponent.java
+++ JMEPlay/JMEPlayCore/src/main/java/com/jmeplay/editor/ui/JMEPlayComponent.java
@@ -10,58 +10,66 @@ import javafx.scene.control.Label;
  * Abstract class to define new component in editor on left or bottom side
  */
 public abstract class JMEPlayComponent {
-	private int priority = Integer.MAX_VALUE;
-	private Align align = Align.LEFT;
+    private int priority = Integer.MAX_VALUE;
+    private Align align = Align.LEFT;
 
-	/** @return priority to place label of component in view */
-	public Integer priority() {
-		return priority;
-	}
+    /**
+     * @return priority to place label of component in view
+     */
+    public Integer priority() {
+        return priority;
+    }
 
-	/**
-	 * Set priority to manage label position in view
-	 *
-	 * @param priority of component
-	 */
-	public void setPriority(int priority) {
-		this.priority = priority;
-	}
+    /**
+     * Set priority to manage label position in view
+     *
+     * @param priority of component
+     */
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
 
-	/** @return alignment of component left or bottom */
-	public Align align() {
-		return align;
-	}
+    /**
+     * @return alignment of component left or bottom
+     */
+    public Align align() {
+        return align;
+    }
 
-	/**
-	 * Change alignment of component
-	 *
-	 * @param align in view
-	 */
-	public void setAlignment(Align align) {
-		this.align = align;
-	}
+    /**
+     * Change alignment of component
+     *
+     * @param align in view
+     */
+    public void setAlignment(Align align) {
+        this.align = align;
+    }
 
-	/** @return name of editor component */
-	public abstract String name();
+    /**
+     * @return name of editor component
+     */
+    public abstract String name();
 
-	/** @return description of editor component */
-	public abstract String description();
+    /**
+     * @return description of editor component
+     */
+    public abstract String description();
 
-	/**
-	 * Label to add in border bar
-	 *
-	 * @return designed label
-	 */
-	public abstract Label label();
+    /**
+     * Label to add in border bar
+     *
+     * @return designed label
+     */
+    public abstract Label label();
 
-	/**
-	 * Main view of component as node
-	 *
-	 * @return view as node
-	 */
-	public abstract Node component();
+    /**
+     * Main view of component as node
+     *
+     * @return view as node
+     */
+    public abstract Node component();
 
-	public enum Align {
-		LEFT, BOTTOM;
-	}
+    public enum Align {
+        LEFT, BOTTOM
+    }
 }
